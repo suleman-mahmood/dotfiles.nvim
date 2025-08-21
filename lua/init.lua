2ffb703409040daede811c71e69944c8dfae357e
Suleman Mahmood <sulemanmahmood99@gmail.com>
2025-08-21 12:24:04 +0500

    add snippets

--- lua/init.lua
+++ lua/init.lua
@@ -2,5 +2,6 @@ local M = {}
 
 M.my_plugin = require("my_plugin")
 M.plugins = require("plugins")
+M.snippets = require("snippets")
 
 return M
---
+++ lua/snippets/init.lua
@@ -0,0 +1,8 @@
+local M = {}
+
+M.dart = require("snippets.dart")
+M.python = require("snippets.python")
+M.rust = require("snippets.rust")
+M.sql = require("snippets.sql")
+
+return M
